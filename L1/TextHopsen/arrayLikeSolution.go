8127674e63aa8451a66f92589d9cc3fd948015b1
Erryo <159823096+Erryo@users.noreply.github.com>
2024-09-25 19:40:00 +0200

    I do not know whether this will work

--- L1/TextHopsen/arrayLikeSolution.go
+++ L1/TextHopsen/arrayLikeSolution.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"unicode"
 )
 
+// Jump means to discard the smallest value
+// Diff means to discard the difference
+// between the bigger value and the jumped value
 func Start() {
 	germanAlphabetMap := map[string]int{
 		"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8,
@@ -25,9 +29,76 @@ func Start() {
 	reader := bufio.NewReader(file)
 
 	pA, pB := 0, 0
-	fmt.Println(pA, pB)
-	read(reader, &pA, germanAlphabetMap)
-	fmt.Println(pA, pB)
+
+	jA := read(reader, &pA, germanAlphabetMap)
+	jB := read(reader, &pB, germanAlphabetMap)
+
+	jumpDistance, firstPlayer := calcWhoseJump(jA, jB)
+	differenceDistance := AbsInt(jA - jB)
+
+	if firstPlayer {
+		discard(reader, &pA, jumpDistance-2)
+		jA = read(reader, &pA, germanAlphabetMap)
+		discard(reader, &pB, differenceDistance)
+		jB = read(reader, &pB, germanAlphabetMap)
+	} else {
+		// Subtracting 1 from jumpDistance because
+		// the pointer is now at the letter after
+		// the one From player 2/B
+		discard(reader, &pB, jumpDistance-1)
+		jB = read(reader, &pB, germanAlphabetMap)
+		// this works because i am one letter further
+		// than expected
+		// smae as above
+		discard(reader, &pA, differenceDistance)
+		jA = read(reader, &pA, germanAlphabetMap)
+	}
+	unread(reader, &pA)
+
+	// Start the Loop
+
+	// An array that stores the values of the last 2 firstPlayer
+	var lastJumps []bool = []bool{firstPlayer}
+	for {
+		jumpDistance, firstPlayer = calcWhoseJump(jA, jB)
+		differenceDistance := AbsInt(jA - jB)
+
+		SmartBoolAppend(lastJumps, firstPlayer)
+
+		if firstPlayer {
+			if lastJumps[0] == firstPlayer {
+				discard(reader, &pA, jumpDistance)
+			} else {
+				discard(reader, &pA, jumpDistance-differenceDistance)
+			}
+
+			jA = read(reader, &pA, germanAlphabetMap)
+			unread(reader, &pA)
+
+			// differenceDistance
+			discard(reader, &pB, differenceDistance)
+			jB = read(reader, &pB, germanAlphabetMap)
+		} else {
+			if lastJumps[0] == firstPlayer {
+				discard(reader, &pB, jumpDistance)
+			} else {
+				discard(reader, &pB, jumpDistance-differenceDistance)
+			}
+
+			jB = read(reader, &pB, germanAlphabetMap)
+			unread(reader, &pB)
+
+			// differenceDistance
+			discard(reader, &pA, differenceDistance)
+			jA = read(reader, &pA, germanAlphabetMap)
+		}
+
+		// If the pos of the current ToJump
+		// is not the current pos
+		// then i need to do
+		// discardDistance = jumpDistance - diff
+		// THIS ASSUMES UNREAD
+	}
 }
 
 // Reades the Current Rune under the cursor and increases the given pointer by 1
@@ -35,11 +106,11 @@ func Start() {
 // until the read rune is a letter then returns the index of the Letter
 func read(reader *bufio.Reader, pX *int, germanAlphabetMap map[string]int) int {
 	*pX += 1
+	// better option, jump by the size of the read rune if it ain't size=1
 	readRune, _, err := reader.ReadRune()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// 182 164 171 159
 	if readRune == 195 {
 		*pX -= 1
 		fmt.Println("FUCKKKKK YOU 195")
@@ -47,5 +118,52 @@ func read(reader *bufio.Reader, pX *int, germanAlphabetMap map[string]int) int {
 	} else if !unicode.IsLetter(readRune) {
 		return read(reader, pX, germanAlphabetMap)
 	}
-	return germanAlphabetMap[string(readRune)]
+	readLetter := strings.ToLower(string(readRune))
+	fmt.Println(readLetter)
+	return germanAlphabetMap[readLetter]
+}
+
+func unread(reader *bufio.Reader, pX *int) {
+	if err := reader.UnreadRune(); err != nil {
+		log.Fatal(err)
+	}
+	*pX -= 1
+}
+
+func discard(reader *bufio.Reader, pX *int, distance int) {
+	_, err := reader.Discard(distance)
+	if err != nil {
+		log.Fatal(distance, err)
+	}
+	*pX += distance
+}
+
+// takes in the jump distances of the 2 Players
+// returns the smallest value and
+// a bool - firstPlayer
+// that indicates whether the first value(Coresponding
+// to the first Player) is the one returned,
+// and that the First Player was the last to jump
+//
+// Is Value B the bigger one
+func calcWhoseJump(a, b int) (int, bool) {
+	fmt.Println("calcWhoseJump:", a, b)
+	if a < b {
+		return a, true
+	}
+	return b, false
+}
+
+func SmartBoolAppend(array []bool, element bool) []bool {
+	if len(array) >= 2 {
+		return []bool{array[1], element}
+	}
+	return append(array, element)
+}
+
+func AbsInt(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
