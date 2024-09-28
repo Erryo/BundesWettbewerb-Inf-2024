fc55fa2b3a4c8dfe0b8a4ab81661b48dda67b924
Erryo <159823096+Erryo@users.noreply.github.com>
2024-09-28 19:16:11 +0200

    Another failure- Switching to fully reading
    Cannot discard because reader.Discard discards bytes not runes, and
    without reading, which is the whole point of doing things this way, it
    is impossible.
    Ex: When the reader discards 3 bytes in will discard 3 bytes and not
    runes so if those 3 expected letters to be jumped is an umlaut then it
    will jump only 2 letters

--- L1/TextHopsen/main.go
+++ L1/TextHopsen/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"strings"
@@ -21,74 +21,90 @@ func main() {
 	//	}
 
 	germanAlphabetMap := map[string]int{
-		"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26,
+		"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8,
+		"i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16,
+		"q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23,
+		"x": 24, "y": 25, "z": 26,
 		"ä": 27, "ö": 28, "ü": 29, "ß": 30,
 	}
 	numberOfMoves := make([]int, NUMBER_OF_PLAYERS)
 
+	data := readFileToString()
 	for playerIndex := range NUMBER_OF_PLAYERS {
 		fmt.Println("-----------------------")
 		// Gonna have to use Channels
-		numberOfMoves[playerIndex] = getPlayersNoOfMoves(playerIndex, germanAlphabetMap)
+		numberOfMoves[playerIndex] = getPlayersNoOfMoves(playerIndex, data, germanAlphabetMap)
 		fmt.Println("-----------------------")
 		fmt.Println(numberOfMoves)
 	}
 }
 
-func getPlayersNoOfMoves(playerNo int, germanAlphabetMap map[string]int) int {
+func readFileToString() string {
 	file, err := os.Open(PATH_TO_FILE)
 	if err != nil {
 		log.Fatal("Failed to open file")
 	}
 	defer file.Close()
 
-	reader := bufio.NewReader(file)
+	var data string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data += scanner.Text()
+	}
+	return data
+}
+
+func getPlayersNoOfMoves(playerNo int, data string, germanAlphabetMap map[string]int) int {
 	playersMoves := 0
+	var locationPointer int
+	lengthOfData := len(data)
 
 	// Not all players start from the same position
 	// they are offset by their number
-	_, err = reader.Discard(playerNo)
+
+	err := increasePointer(&locationPointer, playerNo, lengthOfData)
 	if err != nil {
-		log.Fatal(err)
+		return 0
 	}
 
-	for {
-		jump := read(reader, germanAlphabetMap)
+	for locationPointer < lengthOfData {
+		jump := read(data, &locationPointer, germanAlphabetMap)
 		fmt.Println(" ", jump)
 		playersMoves += 1
 
-		_, err := reader.Discard(jump)
+		err := increasePointer(&locationPointer, jump, lengthOfData)
 		if err != nil {
-			if err != io.EOF {
-				log.Fatal(err, jump)
-			}
 			break
 		}
 	}
 	return playersMoves
 }
 
+func increasePointer(locationPointer *int, amount, lengthOfData int) error {
+	if *locationPointer+amount >= lengthOfData {
+		return errors.New("pointer over the edge")
+	}
+	*locationPointer += amount
+	return nil
+}
+
 // Reades the Current Rune under the cursor and increases the given pointer by 1
 // if it is rune 195 (F*** umlaut BTW) it decreases the pointer and reads again
 // until the read rune is a letter then returns the index of the Letter
-func read(reader *bufio.Reader, germanAlphabetMap map[string]int) int {
+func read(data string, locationPointer *int, germanAlphabetMap map[string]int) int {
 	// better option, jump by the size of the read rune if it ain't size=1
-	readRune, _, err := reader.ReadRune()
-	if err != nil {
-		log.Fatal(err)
-	}
+	readRune := data[*locationPointer]
+	*locationPointer += 1
 
 	if readRune == 195 {
 		fmt.Println("FUCKKKKK YOU 195")
-		return read(reader, germanAlphabetMap)
-	} else if !unicode.IsLetter(readRune) {
-		return read(reader, germanAlphabetMap)
+		return read(data, locationPointer, germanAlphabetMap)
+	} else if !unicode.IsLetter(rune(readRune)) {
+		fmt.Println("NOT A LETTER", string(readRune))
+		return read(data, locationPointer, germanAlphabetMap)
 	}
 
-	err = reader.UnreadRune()
-	if err != nil {
-		log.Fatal(err)
-	}
+	*locationPointer -= 1
 
 	readLetter := strings.ToLower(string(readRune))
 	fmt.Print(readLetter)
